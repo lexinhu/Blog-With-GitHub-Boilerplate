26b9ad96ee24aceb7251ac2efa796b93169dd231
lexinhu <jialna@qq.com>
2020-03-14 04:17:53 +0800

    Update site

--- conf.py
+++ conf.py
@@ -21,7 +21,7 @@ enable_jsdelivr = {
 
 # 站点设置
 site_name = "心湖维基"
-site_logo = "${static_prefix}head.png"
+site_logo = "${static_prefix}logo.png"
 site_build_date = "2019-12-18T16:51+08:00"
 author = "乐心湖"
 email = "jialna@qq.com"
